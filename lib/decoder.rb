b375064e33fa4cc5786cca22644d1afbb52a59ed
Urchmaney <kingsobino@gmail.com>
2020-04-12 07:07:15 +0000

    Add decoder file and decoder rspec file.

---
+++ lib/decoder.rb
@@ -0,0 +1,3 @@
+class Decoder
+  def self.decoder; end
+end
---
+++ spec/decoder_spec.rb
@@ -0,0 +1,11 @@
+require './lib/decoder'
+
+RSpec.describe Decoder do
+  describe '.decode' do
+    it 'it returns protocol length for valid data' do
+    end
+
+    it 'it returns data for the protocol for valid data' do
+    end
+  end
+end
