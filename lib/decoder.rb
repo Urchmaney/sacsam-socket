722d6ad6ca31bb21dc8d0d52e07ab1df6118db85
Urchmaney <kingsobino@gmail.com>
2020-04-14 15:11:23 +0000

    Add decoder stream validity and decode method and test.

--- lib/decoder.rb
+++ lib/decoder.rb
@@ -1,3 +1,17 @@
 class Decoder
-  def self.decoder; end
+  def self.decode(stream)
+    result = { protocol: 0, data: '' }
+    return result unless valid_stream?(stream)
+
+    result[:protocol] = stream[PROTOCOL_INDEX]
+    (DATA_START_INDEX...stream[DATA_LENGTH_INDEX] + DATA_START_INDEX).each do |ele|
+      result[:data] << stream[ele].hex.to_s
+    end
+    result
+  end
+
+  # accept byte array and check validity
+  def self.valid_stream?(stream)
+    !stream.join.match(/^120120\d*9211492110$/).nil?
+  end
 end
--- spec/decoder_spec.rb
+++ spec/decoder_spec.rb
@@ -2,10 +2,25 @@ require './lib/decoder'
 
 RSpec.describe Decoder do
   describe '.decode' do
-    it 'it returns protocol length for valid data' do
+    it 'returns zero protocol and empty data for invalid stream' do
+      data = 'kvjv'
+      expect(Decoder.decode(data.bytes)[:protocol]).to eql(0)
+      expect(Decoder.decode(data.bytes)[:data]).to eql('')
     end
 
-    it 'it returns data for the protocol for valid data' do
+    it 'returns data for the protocol for valid data' do
+    end
+  end
+
+  describe '.isValidStream' do
+    it 'returns false for in-valid stream' do
+      input_stream = 'unknown'
+      expect(Decoder.valid_stream?(input_stream.bytes)).to be_falsey
+    end
+
+    it 'return true for valid stream' do
+      input_stream = 'xx\r\n'
+      expect(Decoder.valid_stream?(input_stream.bytes)).to be_truthy
     end
   end
 end
